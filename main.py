f4e2957395d1b7c410df03c0c923ddaa9837ac33
hkattt <2690133@schoolsnet.act.edu.au>
2020-05-22 10:35:58 +1000

    made the functions more general

--- main.py
+++ main.py
@@ -11,49 +11,64 @@ n = 3
 # calculates the rectangles width
 deltaX = (b - a) / n
 
+
 def f(x):
     """ acts like a mathematical function """
     return (1 / 3) * math.pow(math.e, ((1 / 3) * x))
 
 def lower_rectangle_method(a, b, n):
     """ Uses the lower rectangle method to approximate area """
-    x = 0 # input
-    for _ in range(n):
-        # adding the area of the rectangle to the total area a
-        a += f(x) * deltaX
+    x = a # input
+    # area
+    A = 0
+    for _ in range(0, n):
+        # adding the area of the rectangle to the total area (A)
+        A += f(x) * deltaX
         # increment x
         x += deltaX
-    return a
+    return A
 
 def upper_rectangle_method(a, b, n):
     """ Uses the upper rectangle method to approximate area """
-    x = deltaX # input
-    for _ in range(n):        # adding the area of the rectangle to the total area (a)
-        a += f(x) * deltaX
+    x = a + deltaX # input
+    # area
+    A = 0
+    for _ in range(0, n):        
+        # adding the area of the rectangle to the total area (A)
+        A += f(x) * deltaX
         # increment x
         x += deltaX
-    return a
+    return A
 
 def centered_rectangle_method(a, b, n):
     """ Uses the centered rectangle method to approximate area """
-    x = deltaX / 2 # input
-    for _ in range(n):        # adding the area of the rectangle to the total area (a)
-        a += f(x) * deltaX
+    x = a + (deltaX / 2) # input
+    # area
+    A = 0
+    for _ in range(0, n):        
+        # adding the area of the rectangle to the total area (A)
+        A += f(x) * deltaX
         # increment x
         x += deltaX
-    return a
+    return A
 
 def trapezoid_method(a, b, n):
     """ Uses the trapezoid method to approximate area """
-    x = 0
-    for _ in range(n):        # adding the area of the trapezium to the total area (a)
-        a += (deltaX / 2) * ((f(x) + f(x + deltaX)))
+    x = a
+    # area
+    A = 0
+    for _ in range(0, n):        
+        # adding the area of the trapezium to the total area (A)
+        A += (deltaX / 2) * ((f(x) + f(x + deltaX)))
         # increment x
         x += deltaX
-    return a
+    return A
 
 print("LOWER METHOD:")
 print(str(lower_rectangle_method(a, b, n)) + " units squared")
+#print("ERROR")
+#print((math.pow(math.e, 2) - 1) - lower_rectangle_method(a, b, n))
+
 print("")
 print("UPPER METHOD:")
 print(str(upper_rectangle_method(a, b, n)) + " units squared")
